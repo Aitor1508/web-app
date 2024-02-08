b8f7d12732f1fea273fd2844c31323810b7cd24a
Aitor1508 <aitorviegas@gmail.com>
2024-02-08 13:22:54 +0100

    5.0

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 function canvia_seccio(num_boto) {
-    const menu = document.getElementById("menu");
-    const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
-    for (let i = 1; i < num_botons; i++) {
-        let boto = document.getElementById("boto_" + i);
-        let seccio = document.getElementById("seccio_" + i);
-        if (i == num_boto) {
-            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
-            boto.style.backgroundColor = "#360e95";
-            seccio.style.display = "flex";    // es fa visible la secció activa
-        }
-        else {
-            boto.style.color = "white";    // colors dels botons de seccions inactives
-            boto.style.backgroundColor = "#360e95";
-            seccio.style.display = "none";    // s'oculten les seccions inactives
+    if (validat == true) {
+        const menu = document.getElementById("menu");
+        const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
+        for (let i = 1; i < num_botons; i++) {
+            let boto = document.getElementById("boto_" + i);
+            let seccio = document.getElementById("seccio_" + i);
+            if (i == num_boto) {
+                boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+                boto.style.backgroundColor = "#360e95";
+                seccio.style.display = "flex";    // es fa visible la secció activa
+            }
+            else {
+                boto.style.color = "white";    // colors dels botons de seccions inactives
+                boto.style.backgroundColor = "#360e95";
+                seccio.style.display = "none";    // s'oculten les seccions inactives
+            }
         }
     }
 }
-
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
 let scriptURL = "https://script.google.com/macros/s/AKfycbyvzM5X8ZVN1sJDiF4GQiR8ZZbECUN_PtXz4GHhNdzeYE4QNNM6QONvP0rvN4IfcQe8EA/exec"    // s'ha de substituir la cadena de text per la URL del script
@@ -80,3 +81,4 @@ function tanca_sessio() {
         }
     }
 }
+let 
